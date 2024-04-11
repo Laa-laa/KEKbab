48f84f0ebeb9df5fe6f65b1bf23a76722c019f2d
Salahor <salahor20@gmail.com>
2024-04-11 14:30:09 +0200

    Add tests

---
+++ tests.py
@@ -0,0 +1,27 @@
+import unittest
+
+class TestBorneCommande(unittest.TestCase):
+    def test_affichage_ingredients(self):
+
+        pass
+
+    def test_ajout_ingredient_ticket(self):
+
+        pass
+
+class TestEcranCuisine(unittest.TestCase):
+    def test_affichage_ingredients_commande(self):
+
+        pass
+
+    def test_bouton_effacement_commande(self):
+
+        pass
+
+class TestKebab(unittest.TestCase):
+    def test_calcul_nombre_total(self):
+
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
